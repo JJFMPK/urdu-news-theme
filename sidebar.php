d935a63b4dbd7acbb86276c55e0bacaa8b6ba227
JJFMPK <mansoormehdi5@gmail.com>
2025-06-20 12:44:28 +0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,8 @@
 <aside class="site-sidebar">
+    <div class="sidebar-ad">
+        <img src="https://via.placeholder.com/300x250?text=Sidebar+Ad" alt="Sidebar Ad">
+    </div>
+
     <?php if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
         <?php dynamic_sidebar( 'main-sidebar' ); ?>
     <?php endif; ?>
