fad289de91e0a356f6b3490e7f947adb4260bad8
JJFMPK <mansoormehdi5@gmail.com>
2025-06-20 11:48:27 +0500

    Update page.php

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
+
 <div id="primary" class="content-area">
-    <main id="main" class="site-main">
+    <main id="main" class="site-main" role="main">
         <?php
         while ( have_posts() ) : the_post();
             the_content();
         endwhile;
         ?>
-    </main>
-</div>
+    </main><!-- #main -->
+</div><!-- #primary -->
+
 <?php get_footer(); ?>
