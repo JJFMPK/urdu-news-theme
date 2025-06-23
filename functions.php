9a31099a25975e1a7ff0603cbf9ff8c7dcf35efa
JJFMPK <mansoormehdi5@gmail.com>
2025-06-23 11:53:04 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -33,13 +33,72 @@ register_sidebar(array(
     'after_title'   => '',
 ));
 
-// ACF Options Page
-if( function_exists('acf_add_options_page') ) {
-    acf_add_options_page(array(
-        'page_title'    => 'Theme Options',
-        'menu_title'    => 'Theme Options',
-        'menu_slug'     => 'theme-options',
-        'capability'    => 'edit_posts',
-        'redirect'      => false
-    ));
+// Load Kirki Framework
+if ( ! class_exists( 'Kirki' ) ) {
+    include_once( get_template_directory() . '/inc/kirki/kirki.php' );
 }
+
+// Add Theme Options Section
+function urdu_news_theme_customizer_sections( $wp_customize ) {
+    $wp_customize->add_section( 'theme_options', array(
+        'title'    => __( 'Theme Options', 'urdu-news-theme' ),
+        'priority' => 160,
+    ) );
+}
+add_action( 'customize_register', 'urdu_news_theme_customizer_sections' );
+
+// Kirki Config
+Kirki::add_config( 'urdu_news_theme', array(
+    'capability'    => 'edit_theme_options',
+    'option_type'   => 'theme_mod',
+) );
+
+// Featured Post ID
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'featured_post_id',
+    'label'    => __( 'Featured Post ID', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => '',
+    'priority' => 10,
+] );
+
+// First Block Category ID
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'first_block_category',
+    'label'    => __( 'First Block Category ID', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => '',
+    'priority' => 20,
+] );
+
+// First Block Posts Count
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'first_block_posts',
+    'label'    => __( 'First Block Posts Count', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => 4,
+    'priority' => 30,
+] );
+
+// Second Block Category ID
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'second_block_category',
+    'label'    => __( 'Second Block Category ID', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => '',
+    'priority' => 40,
+] );
+
+// Second Block Posts Count
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'second_block_posts',
+    'label'    => __( 'Second Block Posts Count', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => 4,
+    'priority' => 50,
+] );
