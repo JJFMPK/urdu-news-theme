c919bac09f06af8b80050a8e17a8cf08d7e91f95
JJFMPK <mansoormehdi5@gmail.com>
2025-06-24 10:33:58 +0500

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -7,3 +7,23 @@
         <?php dynamic_sidebar( 'main-sidebar' ); ?>
     <?php endif; ?>
 </aside>
+<div class="widget">
+    <h3 class="widget-title">تازہ خبریں</h3>
+    <ul class="recent-posts-with-thumbs">
+        <?php
+        $recent_posts = new WP_Query(array(
+            'posts_per_page' => 5,
+            'post_status' => 'publish',
+        ));
+        while ($recent_posts->have_posts()) : $recent_posts->the_post(); ?>
+            <li class="recent-post-item">
+                <a href="<?php the_permalink(); ?>">
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <img src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); ?>" alt="<?php the_title(); ?>">
+                    <?php endif; ?>
+                    <span><?php the_title(); ?></span>
+                </a>
+            </li>
+        <?php endwhile; wp_reset_postdata(); ?>
+    </ul>
+</div>
