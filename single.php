00b7923f69b7b2d83ae2e8786140362e0dd333c5
JJFMPK <mansoormehdi5@gmail.com>
2025-06-24 16:15:00 +0500

    Update single.php

--- single.php
+++ single.php
@@ -8,9 +8,18 @@
             <header class="single-header">
                 <h1 class="single-title"><?php the_title(); ?></h1>
 
+                <div class="single-meta">
+                    <span class="post-date"><?php echo get_the_date(); ?></span>
+                    <span class="post-author"><?php the_author(); ?></span>
+                </div>
+
                 <?php if ( has_post_thumbnail() ) : ?>
                     <div class="single-featured-image">
                         <?php the_post_thumbnail('large'); ?>
+                    </div>
+                <?php else : ?>
+                    <div class="single-featured-image">
+                        <img src="https://via.placeholder.com/600x400?text=No+Image" alt="<?php the_title(); ?>">
                     </div>
                 <?php endif; ?>
             </header>
@@ -27,5 +36,3 @@
 </main>
 
 <?php get_footer(); ?>
-
-}
