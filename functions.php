6710fc4c6dc850b18c7ce01fcd52e89d1f3e8673
JJFMPK <mansoormehdi5@gmail.com>
2025-06-24 16:37:27 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -107,3 +107,105 @@ Kirki::add_field( 'urdu_news_theme', [
         ],
     ],
 ]);
+// Register Recent Posts with Thumbs Widget
+class Urdu_News_Recent_Posts_Thumbs extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+            'urdu_news_recent_posts_thumbs',
+            __('Recent Posts with Thumbs (Custom)', 'urdu-news-theme'),
+            array( 'description' => __( 'Display recent posts with thumbnails and category filter.', 'urdu-news-theme' ), )
+        );
+    }
+
+    public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $category = ! empty( $instance['category'] ) ? $instance['category'] : '';
+        $number = ! empty( $instance['number'] ) ? $instance['number'] : 5;
+
+        echo $args['before_widget'];
+        if ( ! empty( $title ) ) {
+            echo $args['before_title'] . $title . $args['after_title'];
+        }
+
+        $query_args = array(
+            'posts_per_page' => $number,
+            'post_status'    => 'publish',
+            'cat'            => $category,
+        );
+
+        $recent_posts = new WP_Query( $query_args );
+
+        if ( $recent_posts->have_posts() ) {
+            echo '<ul class="recent-posts-with-thumbs">';
+            while ( $recent_posts->have_posts() ) : $recent_posts->the_post();
+                echo '<li>';
+                if ( has_post_thumbnail() ) {
+                    echo '<a href="' . get_permalink() . '">';
+                    echo get_the_post_thumbnail( get_the_ID(), 'thumbnail', array( 'style' => 'width:90px; height:90px; object-fit:cover; margin-left:10px; border:1px solid #ddd;' ) );
+                    echo '</a>';
+                }
+                echo '<div class="recent-post-title">';
+                echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
+                echo '</div>';
+                echo '</li>';
+            endwhile;
+            echo '</ul>';
+            wp_reset_postdata();
+        }
+
+        echo $args['after_widget'];
+    }
+
+    public function form( $instance ) {
+        $title = ! empty( $instance['title'] ) ? $instance['title'] : __( 'Recent Posts', 'urdu-news-theme' );
+        $category = ! empty( $instance['category'] ) ? $instance['category'] : '';
+        $number = ! empty( $instance['number'] ) ? $instance['number'] : 5;
+
+        // Title field
+        ?>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
+                   name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text"
+                   value="<?php echo esc_attr( $title ); ?>">
+        </p>
+
+        <!-- Category Dropdown -->
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'category' ) ); ?>"><?php _e( 'Category:' ); ?></label>
+            <?php
+            wp_dropdown_categories( array(
+                'show_option_all' => 'All Categories',
+                'name'            => esc_attr( $this->get_field_name( 'category' ) ),
+                'selected'        => $category,
+                'class'           => 'widefat',
+            ) );
+            ?>
+        </p>
+
+        <!-- Number of posts -->
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>"><?php _e( 'Number of posts:' ); ?></label>
+            <input class="tiny-text" id="<?php echo esc_attr( $this->get_field_id( 'number' ) ); ?>"
+                   name="<?php echo esc_attr( $this->get_field_name( 'number' ) ); ?>" type="number" step="1" min="1"
+                   value="<?php echo esc_attr( $number ); ?>" size="3">
+        </p>
+        <?php
+    }
+
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = sanitize_text_field( $new_instance['title'] );
+        $instance['category'] = sanitize_text_field( $new_instance['category'] );
+        $instance['number'] = absint( $new_instance['number'] );
+
+        return $instance;
+    }
+}
+
+// Register widget
+function register_urdu_news_recent_posts_thumbs() {
+    register_widget( 'Urdu_News_Recent_Posts_Thumbs' );
+}
+add_action( 'widgets_init', 'register_urdu_news_recent_posts_thumbs' );
