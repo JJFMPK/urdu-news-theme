27a6718ca9e262f08ccd2ad0f67b6597b011b6f7
JJFMPK <mansoormehdi5@gmail.com>
2025-06-25 08:45:14 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -17,9 +17,9 @@ function urdu_news_theme_setup() {
 }
 add_action('after_setup_theme', 'urdu_news_theme_setup');
 
-// Enqueue Styles with version bump
+// Enqueue Styles
 function urdu_news_theme_scripts() {
-    wp_enqueue_style('urdu-news-theme-style', get_stylesheet_uri(), array(), '1.2');
+    wp_enqueue_style('urdu-news-theme-style', get_stylesheet_uri(), array(), '1.3');
 }
 add_action('wp_enqueue_scripts', 'urdu_news_theme_scripts');
 
@@ -53,7 +53,7 @@ register_sidebar(array(
     'after_title'   => '',
 ));
 
-// Theme Customizer Section
+// Add Theme Options Section
 function urdu_news_theme_customizer_sections( $wp_customize ) {
     $wp_customize->add_section( 'theme_options', array(
         'title'    => __( 'Theme Options', 'urdu-news-theme' ),
@@ -62,110 +62,48 @@ function urdu_news_theme_customizer_sections( $wp_customize ) {
 }
 add_action( 'customize_register', 'urdu_news_theme_customizer_sections' );
 
-// Kirki Config (if Kirki is used)
-if ( class_exists( 'Kirki' ) ) {
-    Kirki::add_config( 'urdu_news_theme', array(
-        'capability'    => 'edit_theme_options',
-        'option_type'   => 'theme_mod',
-    ));
+// Kirki Config
+Kirki::add_config( 'urdu_news_theme', array(
+    'capability'    => 'edit_theme_options',
+    'option_type'   => 'theme_mod',
+) );
 
-    // Featured Post ID
-    Kirki::add_field( 'urdu_news_theme', [
-        'type'     => 'number',
-        'settings' => 'featured_post_id',
-        'label'    => __( 'Featured Post ID', 'urdu-news-theme' ),
-        'section'  => 'theme_options',
-        'default'  => '',
-        'priority' => 10,
-    ]);
+// Main Grid Category ID (for BBC Grid)
+Kirki::add_field( 'urdu_news_theme', [
+    'type'     => 'number',
+    'settings' => 'main_grid_category_id',
+    'label'    => __( 'Main Grid Category ID', 'urdu-news-theme' ),
+    'section'  => 'theme_options',
+    'default'  => 1,
+    'priority' => 5,
+]);
 
-    // Repeater field for Category Sections
-    Kirki::add_field( 'urdu_news_theme', [
-        'type'        => 'repeater',
-        'label'       => esc_html__( 'Category Sections', 'urdu-news-theme' ),
-        'section'     => 'theme_options',
-        'priority'    => 20,
-        'row_label'   => [
-            'type'  => 'text',
-            'value' => esc_html__( 'Category Section', 'urdu-news-theme' ),
+// Repeater field for Category Sections
+Kirki::add_field( 'urdu_news_theme', [
+    'type'        => 'repeater',
+    'label'       => esc_html__( 'Category Sections', 'urdu-news-theme' ),
+    'section'     => 'theme_options',
+    'priority'    => 20,
+    'row_label'   => [
+        'type'  => 'text',
+        'value' => esc_html__( 'Category Section', 'urdu-news-theme' ),
+    ],
+    'settings'    => 'category_sections',
+    'fields'      => [
+        'section_name' => [
+            'type'    => 'text',
+            'label'   => esc_html__( 'Section Name', 'urdu-news-theme' ),
+            'default' => '',
         ],
-        'settings'    => 'category_sections',
-        'fields'      => [
-            'section_name' => [
-                'type'    => 'text',
-                'label'   => esc_html__( 'Section Name', 'urdu-news-theme' ),
-                'default' => '',
-            ],
-            'category_id' => [
-                'type'    => 'number',
-                'label'   => esc_html__( 'Category ID', 'urdu-news-theme' ),
-                'default' => '',
-            ],
-            'posts_count' => [
-                'type'    => 'number',
-                'label'   => esc_html__( 'Posts Count', 'urdu-news-theme' ),
-                'default' => 5,
-            ],
+        'category_id' => [
+            'type'    => 'number',
+            'label'   => esc_html__( 'Category ID', 'urdu-news-theme' ),
+            'default' => '',
         ],
-    ]);
-}
-
-// Custom Recent Posts With Thumbnail Widget
-class Recent_Posts_Thumb_Widget extends WP_Widget {
-    function __construct() {
-        parent::__construct(
-            'recent_posts_thumb_widget',
-            __('Recent Posts with Thumbs (Custom)', 'urdu-news-theme'),
-            array('description' => __('Shows recent posts with thumbnails', 'urdu-news-theme'))
-        );
-    }
-
-    function widget($args, $instance) {
-        echo $args['before_widget'];
-        if (!empty($instance['title'])) {
-            echo $args['before_title'] . apply_filters('widget_title', $instance['title']) . $args['after_title'];
-        }
-
-        $query = new WP_Query(array(
-            'posts_per_page' => !empty($instance['count']) ? $instance['count'] : 5,
-            'cat' => !empty($instance['cat']) ? $instance['cat'] : '',
-            'post_status' => 'publish'
-        ));
-
-        echo '<ul class="recent-posts-with-thumbs">';
-        while ($query->have_posts()) : $query->the_post();
-            echo '<li><a href="' . get_permalink() . '">';
-            if (has_post_thumbnail()) {
-                echo get_the_post_thumbnail(get_the_ID(), 'thumbnail');
-            }
-            echo '<span class="recent-post-title">' . get_the_title() . '</span></a></li>';
-        endwhile;
-        echo '</ul>';
-        wp_reset_postdata();
-
-        echo $args['after_widget'];
-    }
-
-    function form($instance) {
-        $title = !empty($instance['title']) ? $instance['title'] : __('Recent Posts', 'urdu-news-theme');
-        $count = !empty($instance['count']) ? $instance['count'] : 5;
-        $cat = !empty($instance['cat']) ? $instance['cat'] : '';
-        ?>
-        <p>
-            <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>">
-        </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Number of posts to show:'); ?></label>
-            <input class="tiny-text" id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" type="number" step="1" min="1" value="<?php echo esc_attr($count); ?>">
-        </p>
-        <p>
-            <label for="<?php echo $this->get_field_id('cat'); ?>"><?php _e('Category ID (optional):'); ?></label>
-            <input class="tiny-text" id="<?php echo $this->get_field_id('cat'); ?>" name="<?php echo $this->get_field_name('cat'); ?>" type="number" value="<?php echo esc_attr($cat); ?>">
-        </p>
-        <?php
-    }
-}
-add_action('widgets_init', function() {
-    register_widget('Recent_Posts_Thumb_Widget');
-});
+        'posts_count' => [
+            'type'    => 'number',
+            'label'   => esc_html__( 'Posts Count', 'urdu-news-theme' ),
+            'default' => 5,
+        ],
+    ],
+]);
